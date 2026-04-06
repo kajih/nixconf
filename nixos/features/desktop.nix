b9194f12468d7b8bb4e1406023f6601d63ac6363
KaJIH <kajih@nix>
2026-04-06 16:23:13 +0200

    goxore start

---
+++ nixos/features/desktop.nix
@@ -0,0 +1,69 @@
+{self, ...}: {
+  flake.nixosModules.desktop = {pkgs, ...}: let
+    selfpkgs = self.packages."${pkgs.system}";
+  in {
+    imports = [
+      self.nixosModules.gtk
+
+      self.nixosModules.pipewire
+      self.nixosModules.firefox
+      self.nixosModules.chromium
+    ];
+
+    programs.niri.enable = true;
+    programs.niri.package = selfpkgs.niri;
+
+    # preferences.autostart = [selfpkgs.quickshellWrapped];
+    preferences.autostart = [selfpkgs.noctalia-shell];
+
+    environment.systemPackages = [
+      selfpkgs.terminal
+      pkgs.pcmanfm
+      selfpkgs.noctalia-shell
+    ];
+
+    fonts.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      ubuntu-sans
+      cm_unicode
+      corefonts
+      unifont
+    ];
+
+    fonts.fontconfig.defaultFonts = {
+      serif = ["Ubuntu Sans"];
+      sansSerif = ["Ubuntu Sans"];
+      monospace = ["JetBrainsMono Nerd Font"];
+    };
+
+    time.timeZone = "Europe/Kyiv";
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "uk_UA.UTF-8";
+      LC_IDENTIFICATION = "uk_UA.UTF-8";
+      LC_MEASUREMENT = "uk_UA.UTF-8";
+      LC_MONETARY = "uk_UA.UTF-8";
+      LC_NAME = "uk_UA.UTF-8";
+      LC_NUMERIC = "uk_UA.UTF-8";
+      LC_PAPER = "uk_UA.UTF-8";
+      LC_TELEPHONE = "uk_UA.UTF-8";
+      LC_TIME = "uk_UA.UTF-8";
+    };
+
+    services.upower.enable = true;
+
+    security.polkit.enable = true;
+
+    hardware = {
+      enableAllFirmware = true;
+
+      bluetooth.enable = true;
+      bluetooth.powerOnBoot = true;
+
+      opengl = {
+        enable = true;
+        driSupport32Bit = true;
+      };
+    };
+  };
+}
